0988fa6e129e1991011321368539bfd2bdfe4a3f
hellommmh <hellommmh@gmail.com>
2024-11-25 11:11:33 +0900

    feat : pinia questState에 image도 추가

--- src/stores/questState.js
+++ src/stores/questState.js
@@ -4,12 +4,14 @@ export const useQuestState = defineStore('questState', {
   state: () => ({
     questId: 0,
     questAttractionName: '',
+    questImage:'',
     questSimilarity: 0,
   }),
   actions: {
-    setQuestInfo(questId, title, score) {
+    setQuestInfo(questId, title, image, score) {
       this.questId = questId;
       this.questAttractionName = title;
+      this.questImage = image
       this.questSimilarity = score;
     },
   },
